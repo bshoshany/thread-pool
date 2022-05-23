e9a90997f4b6925a77bee4a55e579daf958e55a7
Chen Yufei <cyfdecyf@gmail.com>
2022-05-23 11:18:49 +0800

    check_task_monitoring: check after sleep.

--- thread_pool_test.cpp
+++ thread_pool_test.cpp
@@ -370,21 +370,25 @@ void check_task_monitoring()
     std::this_thread::sleep_for(std::chrono::milliseconds(300));
     dual_println("After submission, should have: ", n * 3, " tasks total, ", n, " tasks running, ", n * 2, " tasks queued...");
     check(pool.get_tasks_total() == n * 3 && pool.get_tasks_running() == n && pool.get_tasks_queued() == n * 2);
+
     for (ui32 i = 0; i < n; i++)
         release[i] = true;
     std::this_thread::sleep_for(std::chrono::milliseconds(300));
     dual_println("After releasing ", n, " tasks, should have: ", n * 2, " tasks total, ", n, " tasks running, ", n, " tasks queued...");
+    check(pool.get_tasks_total() == n * 2 && pool.get_tasks_running() == n && pool.get_tasks_queued() == n);
+
     for (ui32 i = n; i < n * 2; i++)
         release[i] = true;
-    check(pool.get_tasks_total() == n * 2 && pool.get_tasks_running() == n && pool.get_tasks_queued() == n);
     std::this_thread::sleep_for(std::chrono::milliseconds(300));
     dual_println("After releasing ", n, " more tasks, should have: ", n, " tasks total, ", n, " tasks running, ", 0, " tasks queued...");
     check(pool.get_tasks_total() == n && pool.get_tasks_running() == n && pool.get_tasks_queued() == 0);
+
     for (ui32 i = n * 2; i < n * 3; i++)
         release[i] = true;
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
     dual_println("After releasing the final ", n, " tasks, should have: ", 0, " tasks total, ", 0, " tasks running, ", 0, " tasks queued...");
     check(pool.get_tasks_total() == 0 && pool.get_tasks_running() == 0 && pool.get_tasks_queued() == 0);
+
     dual_println("Resetting pool to ", std::thread::hardware_concurrency(), " threads.");
     pool.reset(std::thread::hardware_concurrency());
 }
